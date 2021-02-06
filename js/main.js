e5720d078fe70f67ed43fe559790eb05494e7ecf
lfares <livia.fares97@gmail.com>
2021-02-06 16:15:16 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function start() {
+    $("#start").hide();
+
+    $("#background-game").append("<div id='player' class='animation-player'></div>");
+    $("#background-game").append("<div id='enemy1' class='animation-enemy1'></div>");
+    $("#background-game").append("<div id='enemy2'></div>");
+    $("#background-game").append("<div id='friend' class='animation-friend'></div>");
+}
